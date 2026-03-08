17543914eb0d9317bf425ef519f52f11b1731a8d
sciAnima <131841156+sciAnima@users.noreply.github.com>
2026-03-08 10:54:35 -0500

    Remove 'br' from Accept-Encoding header

--- scrape_boxing247_text.py
+++ scrape_boxing247_text.py
@@ -75,7 +75,7 @@ def fetch_html() -> str:
         "User-Agent": USER_AGENTS[datetime.now().day % len(USER_AGENTS)],
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Encoding": "gzip, deflate",
         "DNT": "1",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
